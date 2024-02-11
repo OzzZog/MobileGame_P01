6b4d974cc3ddfef3a9170b8e815dc78a918bf9f0
OzzZog <oscarpherzog@gmail.com>
2024-02-10 18:01:43 -0600

    Touch Input Implementation

---
+++ Assets/_Game/Scripts/TouchManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private PlayerInput playerInput;
+
+    private InputAction touchPositionAction;
+    private InputAction touchPressAction;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        touchPositionAction = playerInput.actions["TouchPosition"];
+        touchPressAction = playerInput.actions["TouchPress"];
+    }
+
+    private void OnEnable()
+    {
+        touchPressAction.performed += TouchPressed;
+    }
+
+    private void OnDisable()
+    {
+        touchPressAction.performed -= TouchPressed;
+    }
+
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        Vector3 position = Camera.main.ScreenToWorldPoint(touchPositionAction.ReadValue<Vector2>());
+        position.z = player.transform.position.z;
+        player.transform.position = position;
+
+    }
+}
